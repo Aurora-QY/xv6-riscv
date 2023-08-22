0080b35fe5b530883c1e048f5b63b6e2ea005824
Aurora-QY <1595541692@qq.com>
2023-08-22 12:01:34 +0800

    Lazy PageTable Allocation

---
+++ kernel/sysproc.c
@@ -0,0 +1,109 @@
+#include "types.h"
+#include "riscv.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "proc.h"
+
+uint64
+sys_exit(void)
+{
+  int n;
+  if(argint(0, &n) < 0)
+    return -1;
+  exit(n);
+  return 0;  // not reached
+}
+
+uint64
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+uint64
+sys_fork(void)
+{
+  return fork();
+}
+
+uint64
+sys_wait(void)
+{
+  uint64 p;
+  if(argaddr(0, &p) < 0)
+    return -1;
+  return wait(p);
+}
+
+uint64
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  
+  // 如果要扩展的堆大小超出了可寻址的最大虚拟地址范围，或者小于等于 0，直接返回当前堆大小
+  if (addr + n >= MAXVA || addr + n <= 0)
+    return addr;
+    
+  myproc()->sz += n;  // 虚假地增加size而不物理分配,真正使用该地址时触发缺页中断进行实际页分配
+  
+  // 如果n大于0,则不做操作
+  // 如果n小于0,则需要释放堆内存
+  if(n < 0)
+    uvmdealloc(myproc()->pagetable, addr, myproc()->sz);
+  
+  //if(growproc(n) < 0) //不实际物理分配
+  //  return -1;
+  return addr;
+}
+
+uint64
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+uint64
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+uint64
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
